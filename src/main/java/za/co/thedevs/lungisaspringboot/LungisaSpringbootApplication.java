46259ca548a59bd3bf60f0188a0a2d0897f5ceb1
Tejas Dwarkaram <tejas.dwarkaram@gmail.com>
2022-05-16 16:12:37 +0200

    initial project commit

---
+++ src/main/java/za/co/thedevs/lungisaspringboot/LungisaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package za.co.thedevs.lungisaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LungisaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LungisaSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/thedevs/lungisaspringboot/LungisaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.thedevs.lungisaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LungisaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
